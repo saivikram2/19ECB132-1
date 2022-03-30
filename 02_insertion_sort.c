00c1248ab91fa16a31bea42ebd54d07be918660f
saivikram2 <101540698+saivikram2@users.noreply.github.com>
2022-03-30 10:16:26 +0530

    Update 02_insertion_sort.c

--- 02_insertion_sort.c
+++ 02_insertion_sort.c
@@ -1,11 +1,3 @@
-/* Algorithm
-
-01. Iterate through all elements in the list.
-02. Compare the current element to its predecessor.
-03. If the current element is smaller (greater) than its predecessor, compare it to the elements before.  
-04. Move the greater (smaller) elements one position up to make space for the swapped element.
-*/
-
 # include <stdio.h>
 int main(void) {
   int input[10] = {10, 9, 8, 7, 6, 5, 1, 2, 3, 4};
